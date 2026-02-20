85f4ba117e513f9ff0a85267d55f5a0884998004
Matheus <matheuzhenrik@gmail.com>
2026-02-19 23:56:39 -0300

    chore: format w/ black

--- app/domain/repositories/hockey_repository.py
+++ app/domain/repositories/hockey_repository.py
@@ -3,13 +3,14 @@ from app.domain.models.hockey_team import HockeyTeam
 
 from sqlalchemy.dialects.postgresql import insert
 
+
 def bulk_insert_hockey(db: Session, teams: list[dict]):
     if not teams:
         return
 
     stmt = insert(HockeyTeam).values(teams)
 
-    stmt = stmt.on_conflict_do_nothing(index_elements=['team_name', 'year'])
+    stmt = stmt.on_conflict_do_nothing(index_elements=["team_name", "year"])
 
     try:
         db.execute(stmt)
--- app/domain/repositories/oscar_repository.py
+++ app/domain/repositories/oscar_repository.py
@@ -3,13 +3,14 @@ from app.domain.models.oscar_film import OscarFilm
 
 from sqlalchemy.dialects.postgresql import insert
 
+
 def bulk_insert_oscar(db: Session, films: list[dict]):
     if not films:
         return
 
     stmt = insert(OscarFilm).values(films)
 
-    stmt = stmt.on_conflict_do_nothing(index_elements=['title', 'year'])
+    stmt = stmt.on_conflict_do_nothing(index_elements=["title", "year"])
 
     try:
         db.execute(stmt)
